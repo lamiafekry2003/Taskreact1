7f888b729232137b4827c5b6e24678394697d362
DESKTOP-5EMQ9IB\user <Lamia.fekry2003@gmail.com>
2024-02-12 21:55:28 +0200

    change

--- src/Notfound/Notfound.jsx
+++ src/Notfound/Notfound.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-
 export default function Notfound() {
   return (
     <div>
         <div className="container">
             <div className="row">
-                <div className="col-md-12">
-                    <h1 style={{height:"600px",color:"black", margin:"10% auto"}}> Page Not Found sorry </h1>
+                <div className="col-md-12 d-flex justify-content-center align-item-center ">
+                  <h1 style={{height:"600px",color:"black", margin:"30% auto"}}><span style={{color:'grey'}}> Oppes :</span>Page Not Found sorry </h1>
                 </div>
             </div>
         </div>
